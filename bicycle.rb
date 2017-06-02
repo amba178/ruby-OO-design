1d717592204ef81d22486b2cc8020df3882bc376
Salem Amba <salemamba@gmail.com>
2017-06-02 14:33:00 -0700

    adds module

--- bicycle.rb
+++ bicycle.rb
@@ -1,10 +1,16 @@
+# require './schedule'
+require 'date'
+require './schedulable'
+
 class Bicycle 
-	attr_reader :size, :chain, :tire_size 
+	include Schedulable
+
+	attr_reader :size, :chain, :tire_size
 
 	def initialize(args={})
 		@size = args[:size]
 		@chain = args[:chain] || default_chain 
-		@tire_size = args[:tire_size] || default_tire_size
+		@tire_size = args[:tire_size]  || default_tire_size
 		post_initialize(args)
 	end
 
@@ -32,4 +38,31 @@ class Bicycle
 		{}
 	end
 
+	# # Return true if this bicycle is available 
+	# # during this (now Bicycle) interval.
+	# def schedulable?(start_date, end_date)
+	# 	!scheduled?(start_date - lead_days, end_date)
+	# end
+
+	# def scheduled?(start_date, end_date)
+	# 	schedule.scheduled?(self, start_date, end_date)
+	# end
+
+	# moved to schedulable module
+
+	def lead_days
+		1
+	end
+
 end
+
+
+
+
+
+starting = Date.parse("2015/09/04")
+ending = Date.parse("2015/09/10")
+
+b = Bicycle.new
+puts b.schedulable?(starting, ending)
+
